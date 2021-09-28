016f346e1dbe52aed3d886ccdd0e4862a554c6e0
unknown <lonelylok101@gmail.com>
2021-09-28 00:04:30 -0700

    starting react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p className='centered'>Homepage working in progress</p>
+    </div>
+  );
+}
+
+export default App;
