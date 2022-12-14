405e23ae75906a3ebc34136cd833ac2120bb1b74
Hei Lok Yu <lonelylok101@gmail.com>
2022-12-13 21:00:26 -0800

    wip

--- src/MonsterHunterWorldVideo.tsx
+++ src/MonsterHunterWorldVideo.tsx
@@ -1,18 +1,14 @@
-import { VideoMenuBase } from "./VideoMenuBase"
-const videoMenuBase = new VideoMenuBase({
-    title: 'Monster Hunter World',
-    data: [
-        {id: 1, title: 'Long Sword, Alatreon', thumbnailLink: 'https://img.youtube.com/vi/c9rNif89naI/0.jpg', videoLink: ''},
-        {id: 2, title: 'Long Sword, Kirin', thumbnailLink: 'https://img.youtube.com/vi/TMTJ1AbnAAI/0.jpg', videoLink: ''},
-        {id: 3, title: 'Long Sword, Teostra', thumbnailLink: 'https://img.youtube.com/vi/qi0ZTAlbyis/0.jpg', videoLink: ''},
-        {id: 4, title: 'Long Sword, Ruiner Nergigante', thumbnailLink: 'https://img.youtube.com/vi/OBNxqgdpxvk/0.jpg', videoLink: ''}
-    ]
-})
-
+import  VideoMenuBase from "./VideoMenuBase"
+const data = [
+    {id: 1, title: 'Long Sword, Alatreon', thumbnailLink: 'https://img.youtube.com/vi/c9rNif89naI/0.jpg', videoLink: 'https://www.youtube.com/embed/c9rNif89naI'},
+    {id: 2, title: 'Long Sword, Kirin', thumbnailLink: 'https://img.youtube.com/vi/TMTJ1AbnAAI/0.jpg', videoLink: 'https://www.youtube.com/embed/TMTJ1AbnAAI'},
+    {id: 3, title: 'Long Sword, Teostra', thumbnailLink: 'https://img.youtube.com/vi/qi0ZTAlbyis/0.jpg', videoLink: 'https://www.youtube.com/embed/qi0ZTAlbyis'},
+    {id: 4, title: 'Long Sword, Ruiner Nergigante', thumbnailLink: 'https://img.youtube.com/vi/OBNxqgdpxvk/0.jpg', videoLink: 'https://www.youtube.com/embed/OBNxqgdpxvk'}
+];
 const MonsterHunterWorldVideo = () => {
     return(
         <div>
-            {videoMenuBase.run()}
+            <VideoMenuBase mainTitle="Monster Hunter World" data={data}></VideoMenuBase>
         </div>
     )
 } 
--- src/VideoMenuBase.tsx
+++ src/VideoMenuBase.tsx
@@ -1,38 +1,74 @@
-import { Typography, Card, CardMedia, Container, CardContent } from '@mui/material';
+import { Typography, Card, CardMedia, Container, CardContent, CardActionArea, Modal, Box } from '@mui/material';
+import * as React from 'react';
 import './VideoMenuBase.css';
-export class VideoMenuBase {
-    private title: string;
-    private data: {id: number; thumbnailLink: string; videoLink: string; title: string}[];
-    constructor({
-        title,
-        data
-    }: {
-        title: string;
-        data: {id: number; thumbnailLink: string; videoLink: string; title: string}[];
-    }) {
-        this.title = title;
-        this.data = data;
-    }
 
-    run() {
-        return (<div>
-            <Typography align='center' variant='h4'>{this.title}</Typography>
-            <Container >
-                <div className="customContainer">
-                    {this.data.map(({thumbnailLink, title})=>{
-                        return(
-                            <Card>
-                        <CardMedia component="img" image={thumbnailLink} alt={title}></CardMedia>
-                        <CardContent>
-                            <Typography gutterBottom component="div">
-                                Title: {title}
-                            </Typography>
-                        </CardContent>
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 1280,
+    bgcolor: 'black',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+const videoWidth = 1280;
+const videoHeight = 720;
+
+const VideoMenuBase = ({
+    mainTitle,
+    data
+}: {
+    mainTitle: string;
+    data: { id: number; thumbnailLink: string; videoLink: string; title: string }[];
+}) =>{
+const [open, setOpen] = React.useState(false);
+const [videoLink, setVideoLink] = React.useState('');
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setVideoLink('')
+    setOpen(false)
+  };
+  const handleClick = (videoLink: string) => {
+    handleOpen()
+    setVideoLink(videoLink)
+  }
+  return(<div>
+    <Typography align='center' variant='h4'>{mainTitle}</Typography>
+    <Container >
+        <div className="customContainer">
+            {data.map(({ thumbnailLink, title, videoLink }) => {
+                return (
+                    <Card>
+                        <CardActionArea onClick={() => handleClick(videoLink)}>
+                            <CardMedia component="img" image={thumbnailLink} alt={title}>
+                            </CardMedia>
+                            <CardContent>
+                                <Typography gutterBottom component="div">
+                                    Title: {title}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
                     </Card>
-                        )
-                    })}
-                </div>
-            </Container>
-        </div>)
-    }
+                )
+            })}
+            <Modal
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                            >
+                                <Box sx={style}>
+                                    <Typography id="modal-modal-description">
+                                    {(<iframe width={videoWidth} height={videoHeight} src={videoLink} title="YouTube video player"frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>)}
+                                    </Typography>
+                                </Box>
+                            </Modal>
+        </div>
+    </Container>
+</div>)
 }
+
+export default VideoMenuBase
