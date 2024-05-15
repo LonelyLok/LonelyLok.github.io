33c5baa2bd113666f3a5f148e31dcfe2e4d2db11
LonelyLok <40349145+LonelyLok@users.noreply.github.com>
2024-05-14 21:58:49 -0700

    small changes (#19)

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { useSpring, animated } from 'react-spring'
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+import profilePictures from './assets/profile.jpg';
 import './App.css';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Flip from './Flip';
-// import CustomizedAccordions from './CustomizedAccordions';
-import Typography from '@mui/material/Typography';
-import { List, ListItem, ListItemIcon } from '@mui/material';
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  Avatar,
+  Typography,
+  Link,
+  Box,
+} from '@mui/material';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import OndemandVideoSharpIcon from '@mui/icons-material/OndemandVideoSharp';
-// import CustomizedModal from './CustomizedModal';
-import Link from '@mui/material/Link';
 
 const globalFontType = 'Arial';
 
@@ -18,92 +23,157 @@ const Home = () => {
   const styles = useSpring({
     loop: false,
     from: { opacity: 0, color: '#333' },
-    to: { opacity: 1, color: 'white' }
-  })
+    to: { opacity: 1, color: 'white' },
+  });
 
   const cardBuilder = (l: Array<any>) => {
     return (
       <Card sx={{ minWidth: 275, minHeight: 275, background: '#424242' }}>
         <CardContent>
           {l.map((s: any) => (
-            <Typography sx={{ fontSize: 14, fontFamily: {globalFontType} }} color="white" gutterBottom>
+            <Typography
+              sx={{ fontSize: 14, fontFamily: { globalFontType } }}
+              color='white'
+              gutterBottom
+            >
               {s}
             </Typography>
           ))}
         </CardContent>
-        <CardContent sx={{position: 'absolute', bottom: 0, right: 0}}><Typography sx={{ fontSize: 9, fontFamily: {globalFontType} }} color="white" gutterBottom>
-             Click to Filp
-            </Typography></CardContent>
+        <CardContent sx={{ position: 'absolute', bottom: 0, right: 0 }}>
+          <Typography
+            sx={{ fontSize: 9, fontFamily: { globalFontType } }}
+            color='white'
+            gutterBottom
+          >
+            Click to Filp
+          </Typography>
+        </CardContent>
       </Card>
-    )
-  }
+    );
+  };
 
   const iconLabelMap = {
-    'dot': <FiberManualRecordIcon style={{ fontSize: 'small', minWidth: '20px', color: 'white' }} />,
-    'video': <OndemandVideoSharpIcon style={{ fontSize: 'small', minWidth: '20px', color: 'white' }} />
-  }
-  // const gameGenres = ['Action RPG', 'Soulslike'];
-  // const pcSpec = ['MB: ASRock X570 Phantom Gaming 4', 'CPU: AMD Ryzen 5 3600', 'GPU: AMD 5700XT', 'CASE: NZXT H510', 'PSU: CORSAIR TX-M Series 550W', 'RAM: 32GB', 'SSD: 500GB', 'HDD: 1TB'];
-  // const typographyListBuilder = (l: Array<any>, iconLabel: any) => (
-  //   <Typography>
-  //     {l.map((s: any) => (<ListItem sx={{ fontFamily: {globalFontType}, fontSize: 12 }} disablePadding><ListItemIcon style={{ minWidth: '10px' }}>{iconLabel}</ListItemIcon>{s}</ListItem>))}
-  //   </Typography>
-  // );
-  // const [videoWidth, videoHeight] = [1280, 720]
-  // const videoPlayList = [
-  //   <CustomizedModal title='Lost Ark 1385ilv SH Velganos' video={(<iframe width={videoWidth} height={videoHeight} src="https://www.youtube.com/embed/Gud1JaBKnkQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>)} />,
-  //   <CustomizedModal title='Monster Hunter Rise PC Demo' video={(<iframe width={videoWidth} height={videoHeight} src="https://www.youtube.com/embed/EYW0qSlNZvg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>)} />
-  // ]
-  // const accordionList = [{ title: 'Favorite video game genre', summary: typographyListBuilder(gameGenres, iconLabelMap['dot']) },
-  // { title: 'Computer spec', summary: typographyListBuilder(pcSpec, iconLabelMap['dot']) },
-  // { title: 'Game record video', summary: typographyListBuilder(videoPlayList, iconLabelMap['video']) }]
+    dot: (
+      <FiberManualRecordIcon
+        style={{ fontSize: 'small', minWidth: '20px', color: 'white' }}
+      />
+    ),
+    video: (
+      <OndemandVideoSharpIcon
+        style={{ fontSize: 'small', minWidth: '20px', color: 'white' }}
+      />
+    ),
+  };
 
-  const codeProjectBuilder = ({ title, link }: { title: string; link: string }) => [
-    <ListItem sx={{ fontFamily: {globalFontType}, fontSize: 12, pl: 0 }} >
-      <ListItemIcon style={{ minWidth: '10px' }}>{iconLabelMap['dot']}</ListItemIcon>
-      <Link sx={{ fontSize: '10px', color: 'white', 'textDecoration': 'underline' }} href={link}>{title}</Link>
-    </ListItem>
-  ]
+  const codeProjectBuilder = ({
+    title,
+    link,
+  }: {
+    title: string;
+    link: string;
+  }) => [
+    <ListItem sx={{ fontFamily: { globalFontType }, fontSize: 12, pl: 0 }}>
+      <ListItemIcon style={{ minWidth: '10px' }}>
+        {iconLabelMap['dot']}
+      </ListItemIcon>
+      <Link
+        sx={{ fontSize: '10px', color: 'white', textDecoration: 'underline' }}
+        href={link}
+      >
+        {title}
+      </Link>
+    </ListItem>,
+  ];
 
   return (
     <animated.div style={styles}>
-      <p className='centered'>Hi, my name is Henry and welcome.</p>
-      <div className="container">
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Typography variant='h4'>
+          Hi, my name is Henry. Hope you have a good day.
+        </Typography>
+        <Avatar
+          sx={{ ml: 2, height: 128, width: 128 }} // Add margin-left to create space between title and avatar
+          src={profilePictures}
+          alt="Henry's Profile Picture"
+        />
+      </Box>
+      <div className='container'>
         <Flip
           front={cardBuilder(['About Me'])}
-          back={cardBuilder(['Name: Henry Yu', `Age: ${new Date().getFullYear() - 1996}`, 'Hobbies: Sleep, play video games', 'Favorite food: Rice', 'Favorite drink: Green tea', <Link sx={{color: 'white', 'textDecoration': 'underline'}} href='#/aboutmetest'>And More</Link>])}
+          back={cardBuilder([
+            'Name: Henry Yu',
+            `Age: ${new Date().getFullYear() - 1996}`,
+            'Hobbies: Sleep, play video games',
+            'Favorite food: Rice',
+            'Favorite drink: Green tea',
+            <Link
+              sx={{ color: 'white', textDecoration: 'underline' }}
+              href='#/aboutmetest'
+            >
+              And More
+            </Link>,
+          ])}
         ></Flip>
         <Flip
           front={cardBuilder(['Work Related'])}
-          back={cardBuilder(['Current job: Software Engineer', `Experience: ${new Date().getFullYear() - 2019} years`, 'Fields: Web, machine learning', 'Tech stack: Typescript, NodeJS, MySQL, AWS, Docker, Python',<Link sx={{color: 'white', 'textDecoration': 'underline'}} href='#/resume'>Resume</Link>])}
+          back={cardBuilder([
+            'Current job: Software Engineer',
+            `Experience: ${new Date().getFullYear() - 2019} years`,
+            'Fields: Web, machine learning',
+            'Tech stack: Typescript, NodeJS, MySQL, AWS, Docker, Python',
+            <Link
+              sx={{ color: 'white', textDecoration: 'underline' }}
+              href='#/resume'
+            >
+              Resume
+            </Link>,
+          ])}
         ></Flip>
         <List>
-        <ListItem>
-            <ListItemIcon style={{ minWidth: '10px' }}>{iconLabelMap['dot']}</ListItemIcon>
+          <ListItem>
+            <ListItemIcon style={{ minWidth: '10px' }}>
+              {iconLabelMap['dot']}
+            </ListItemIcon>
             <Typography>
-              <Link href="#/dayoflifegame">My day of life</Link>
+              <Link href='#/dayoflifegame'>My day of life</Link>
             </Typography>
           </ListItem>
           <ListItem>
-            <ListItemIcon style={{ minWidth: '10px' }}>{iconLabelMap['dot']}</ListItemIcon>
+            <ListItemIcon style={{ minWidth: '10px' }}>
+              {iconLabelMap['dot']}
+            </ListItemIcon>
             <Typography>
-              <Link href="#/videogamefootagemenu">Video game footage archive</Link>
+              <Link href='#/videogamefootagemenu'>
+                Video game footage archive
+              </Link>
             </Typography>
           </ListItem>
         </List>
         <Flip
           front={cardBuilder(['Side projects'])}
           back={cardBuilder([
-            codeProjectBuilder({ title: 'job helper', link: 'https://github.com/LonelyLok/job-helper' }),
-            codeProjectBuilder({ title: 'Split helper', link: '#/splithelper' }),
-            codeProjectBuilder({ title: 'Twitch chat bot example', link: 'https://github.com/LonelyLok/twitch-chat-bot' }),
-            codeProjectBuilder({ title: 'This webpage', link: 'https://github.com/LonelyLok/LonelyLok.github.io' }),
+            codeProjectBuilder({
+              title: 'job helper',
+              link: 'https://github.com/LonelyLok/job-helper',
+            }),
+            codeProjectBuilder({
+              title: 'Split helper',
+              link: '#/splithelper',
+            }),
+            codeProjectBuilder({
+              title: 'Twitch chat bot example',
+              link: 'https://github.com/LonelyLok/twitch-chat-bot',
+            }),
+            codeProjectBuilder({
+              title: 'This webpage',
+              link: 'https://github.com/LonelyLok/LonelyLok.github.io',
+            }),
           ])}
         ></Flip>
       </div>
     </animated.div>
-  )
+  );
+};
 
-}
-
-export default Home
+export default Home;
